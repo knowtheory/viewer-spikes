06361d0ecc3fb038fa4169a5e3791423e40f08ec
Ted Han <ted@knowtheory.net>
2013-11-12 15:55:09 -0600

    Add a little gutter.

--- experiments/page_renderer/public/assets/templates.js
+++ experiments/page_renderer/public/assets/templates.js
@@ -1,7 +1,7 @@
 (function(){
 window.JST = window.JST || {};
 
-window.JST['page'] = DC._.template('<div class="page" style="width: <%= page.width %>px; height: <%= page.height %>px;">\n  <%= page.pageNumber %>\n</div>\n');
+window.JST['page'] = DC._.template('<div class="page_number">p. <%= page.pageNumber %></div>\n<div class="page" style="width: <%= page.width %>px; height: <%= page.height %>px;">\n  \n</div>\n');
 window.JST['page_set'] = DC._.template('');
 window.JST['viewer'] = DC._.template('<div class="pages"></div>\n<div class="footer"></div>\n');
 })();
