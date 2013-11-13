5e32097fd2ca826cab0b1ec319d20399b53d793f
Ted Han <ted@knowtheory.net>
2013-11-13 16:55:18 -0600

    Page loader.

--- experiments/page_renderer/public/assets/viewer.js
+++ experiments/page_renderer/public/assets/viewer.js
@@ -12802,14 +12802,34 @@ DC.view.Viewer = DC.Backbone.View.extend({
 
 DC.view.PageList = DC.Backbone.View.extend({
   className: 'pages',
-
+  
   initialize: function(options) {
+    this.currentPageIndex = (this.currentPageIndex || 0);
+    this.throttledLoadVisiblePages = DC._.throttle(DC._.bind(this.loadVisiblePages, this), 100);
   },
   
+  events: { 'scroll': 'throttledLoadVisiblePages' },
+
   render: function() {
     this.pageViews = this.collection.map( function( pageModel ){ return new DC.view.Page({model: pageModel}); } ) ;
     this.$el.html( DC._.map(this.pageViews, function(view){ return view.render().el; }) );
     return this;
+  },
+  
+  loadVisiblePages: function(e){
+    this.identifyCurrentPage();
+    var floor   = this.currentPage < 10                ? 1 : (this.currentPage - 10);
+    var ceiling = this.currentPage > this.collection.size() ? this.collection.size() : (this.currentPage + 10);
+    var range   = DC._.range(floor, ceiling);
+    this.loadPages(range);
+  },
+  
+  identifyCurrentPage: function() {
+    this.currentPage = 12;
+  },
+  
+  loadPages: function(pageNumbers) {
+    DC._.each( pageNumbers, function(pageNumber){ this.pageViews[pageNumber-1].loadImage(); }, this);
   }
 });
 
