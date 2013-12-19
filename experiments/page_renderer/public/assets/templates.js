a0348072614cd6a17eae7906eae2fad6b81543d9
Ted Han <ted@knowtheory.net>
2013-12-18 21:01:00 -0600

    Fix positioning in overview template.

--- experiments/page_renderer/public/assets/templates.js
+++ experiments/page_renderer/public/assets/templates.js
@@ -1,7 +1,7 @@
 (function(){
 window.JST = window.JST || {};
 
-window.JST['overview'] = DC._.template('<div class="page_mark ui-slider-handle">\n  <div class="mark_text">page <span class="page_number">1</span></div>\n</div>\n<% pages.each(function(page) { \n  var pageNumber = page.get(\'pageNumber\');\n  var pageCount  = pages.length;\n%>\n<div class="page<%= (pageNumber % 2) ? \' even\' : \'\' %>" style="width: 100%;position: absolute; top:<%= (100*pageNumber/pageCount) %>%; height:<%= (100/pageCount) %>%"></div>\n<% }); %>\n');
+window.JST['overview'] = DC._.template('<div class="page_mark ui-slider-handle">\n  <div class="mark_text">page <span class="page_number">1</span></div>\n</div>\n<% pages.each(function(page, pageIndex) { \n  var pageCount  = pages.length;\n%>\n<div class="page<%= (pageIndex % 2) ? \' even\' : \'\' %>" style="width: 100%;position: absolute; top:<%= (100*pageIndex/pageCount) %>%; height:<%= (100/pageCount) %>%"></div>\n<% }); %>\n');
 window.JST['page'] = DC._.template('<div class="page_number">p. <%= page.pageNumber %></div>\n<div class="header"></div>\n<div class="matte" style="width: <%= page.width %>px; height: <%= page.height %>px;">\n</div>\n');
 window.JST['pages'] = DC._.template('');
 window.JST['renderer'] = DC._.template('<div class="backdrop">\n  <div class="pages"></div>\n</div>\n<div class="overview"></div>\n');
