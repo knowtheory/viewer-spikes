46c8a141b34c47043eaf5ed3cbc092c1e3013a1a
Ted Han <ted@knowtheory.net>
2014-01-13 11:23:52 -0500

    update packaged viewer

--- experiments/page_renderer/public/assets/viewer.js
+++ experiments/page_renderer/public/assets/viewer.js
@@ -11915,6 +11915,166 @@ $.widget( "ui.slider", $.ui.mouse, {
 
 }(jQuery));
 
+/*!
+ * jQuery UI Touch Punch 0.2.2
+ *
+ * Copyright 2011, Dave Furfero
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Depends:
+ *  jquery.ui.widget.js
+ *  jquery.ui.mouse.js
+ */
+(function ($) {
+
+  // Detect touch support
+  $.support.touch = 'ontouchend' in document;
+
+  // Ignore browsers without touch support
+  if (!$.support.touch) {
+    return;
+  }
+
+  var mouseProto = $.ui.mouse.prototype,
+      _mouseInit = mouseProto._mouseInit,
+      touchHandled;
+
+  /**
+   * Simulate a mouse event based on a corresponding touch event
+   * @param {Object} event A touch event
+   * @param {String} simulatedType The corresponding mouse event
+   */
+  function simulateMouseEvent (event, simulatedType) {
+
+    // Ignore multi-touch events
+    if (event.originalEvent.touches.length > 1) {
+      return;
+    }
+
+    event.preventDefault();
+
+    var touch = event.originalEvent.changedTouches[0],
+        simulatedEvent = document.createEvent('MouseEvents');
+    
+    // Initialize the simulated mouse event using the touch event's coordinates
+    simulatedEvent.initMouseEvent(
+      simulatedType,    // type
+      true,             // bubbles                    
+      true,             // cancelable                 
+      window,           // view                       
+      1,                // detail                     
+      touch.screenX,    // screenX                    
+      touch.screenY,    // screenY                    
+      touch.clientX,    // clientX                    
+      touch.clientY,    // clientY                    
+      false,            // ctrlKey                    
+      false,            // altKey                     
+      false,            // shiftKey                   
+      false,            // metaKey                    
+      0,                // button                     
+      null              // relatedTarget              
+    );
+
+    // Dispatch the simulated event to the target element
+    event.target.dispatchEvent(simulatedEvent);
+  }
+
+  /**
+   * Handle the jQuery UI widget's touchstart events
+   * @param {Object} event The widget element's touchstart event
+   */
+  mouseProto._touchStart = function (event) {
+
+    var self = this;
+
+    // Ignore the event if another widget is already being handled
+    if (touchHandled || !self._mouseCapture(event.originalEvent.changedTouches[0])) {
+      return;
+    }
+
+    // Set the flag to prevent other widgets from inheriting the touch event
+    touchHandled = true;
+
+    // Track movement to determine if interaction was a click
+    self._touchMoved = false;
+
+    // Simulate the mouseover event
+    simulateMouseEvent(event, 'mouseover');
+
+    // Simulate the mousemove event
+    simulateMouseEvent(event, 'mousemove');
+
+    // Simulate the mousedown event
+    simulateMouseEvent(event, 'mousedown');
+  };
+
+  /**
+   * Handle the jQuery UI widget's touchmove events
+   * @param {Object} event The document's touchmove event
+   */
+  mouseProto._touchMove = function (event) {
+
+    // Ignore event if not handled
+    if (!touchHandled) {
+      return;
+    }
+
+    // Interaction was not a click
+    this._touchMoved = true;
+
+    // Simulate the mousemove event
+    simulateMouseEvent(event, 'mousemove');
+  };
+
+  /**
+   * Handle the jQuery UI widget's touchend events
+   * @param {Object} event The document's touchend event
+   */
+  mouseProto._touchEnd = function (event) {
+
+    // Ignore event if not handled
+    if (!touchHandled) {
+      return;
+    }
+
+    // Simulate the mouseup event
+    simulateMouseEvent(event, 'mouseup');
+
+    // Simulate the mouseout event
+    simulateMouseEvent(event, 'mouseout');
+
+    // If the touch interaction did not move, it should trigger a click
+    if (!this._touchMoved) {
+
+      // Simulate the click event
+      simulateMouseEvent(event, 'click');
+    }
+
+    // Unset the flag to allow other widgets to inherit the touch event
+    touchHandled = false;
+  };
+
+  /**
+   * A duck punch of the $.ui.mouse _mouseInit method to support touch events.
+   * This method extends the widget with bound touch event handlers that
+   * translate touch events to mouse events and pass them to the widget's
+   * original mouse event handling methods.
+   */
+  mouseProto._mouseInit = function () {
+    
+    var self = this;
+
+    // Delegate the touch handlers to the widget's element
+    self.element
+      .bind('touchstart', $.proxy(self, '_touchStart'))
+      .bind('touchmove', $.proxy(self, '_touchMove'))
+      .bind('touchend', $.proxy(self, '_touchEnd'));
+
+    // Call the original $.ui.mouse init method
+    _mouseInit.call(self);
+  };
+
+})(jQuery);
 //     Underscore.js 1.5.2
 //     http://underscorejs.org
 //     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -14912,6 +15072,9 @@ DC.view.DocumentViewer = DC.Backbone.View.extend({
     'click .footer .up'   : 'previousPage',
     'click .footer .down' : 'nextPage',
     'click .footer .menu' : 'menu',
+    'dblclick .footer .up'   : 'previousPage',
+    'dblclick .footer .down' : 'nextPage',
+    'dblclick .footer .menu' : 'menu'
   },
   
   initialize: function(options) {
@@ -14983,6 +15146,21 @@ DC._.extend(DV, {
   documents: new DC.model.DocumentSet(),
   viewers: {}
 });
+/*
+  When do notes get rendered/loaded?
+
+  when a new note is created
+  when a note is edited
+  when a page is loaded
+  
+  Note view should manage user interaction on itself
+  Note view should track changes to its model
+  
+  An overlay should keep track of whether it, and its children should be loaded or not.
+  
+  Should notes be an extractable/reusable component?
+*/
+
 DC.view.Note = DC.Backbone.View.extend({
   className: 'note'
 });
@@ -14995,11 +15173,31 @@ DC.view.PageOverlay = DC.Backbone.View.extend({
   className: 'overlay',
   initialize: function(options) {
     this.page = options.page;
-    this.listenTo(this.page, 'load', load);
-    this.listenTo(this.page)
+    this.listenTo(this.page, 'loaded', this.load);
+    this.listenTo(this.page, 'unloaded', this.unload);
   }
 });
 
+DC.view.NoteOverlay = DC.view.PageOverlay.extend({
+  className: 'noteOverlay',
+  initialize: function(options) {
+    // javascript "inheritance" my foot. (see: https://github.com/jashkenas/backbone/pull/787 )
+    DC.view.PageOverlay.prototype.initialize.call(this, options);
+    this.collection = this.page.model.notes;
+  },
+  
+  render: function() {
+    return this.$el;
+  },
+  
+  load: function() {
+    
+  },
+  
+  unload: function() {
+    
+  }
+});
 DC.view.Overview = DC.Backbone.View.extend({
   className: 'overview',
   
@@ -15138,15 +15336,10 @@ DC.view.PageList = DC.Backbone.View.extend({
   className: 'pages',
   
   initialize: function(options) {
-    //this.loadVisiblePages          = DC._.bind(this.loadVisiblePages, this);
-    //this.throttledLoadVisiblePages = DC._.throttle(this.loadVisiblePages, this.SCROLL_THROTTLE);
-    this.announceScroll            = DC._.bind(DC._.throttle(this.announceScroll, this.SCROLL_THROTTLE), this);
-    
     this.matteHeight = 0;
     this.initializeSubviews();
 
     this.listenTo(this.collection, 'reset', this.rebuild);
-    //this.on('scroll', this.loadVisiblePages);
   },
 
   rebuild: function() {
@@ -15220,7 +15413,7 @@ DC.view.PageList = DC.Backbone.View.extend({
 
 DC.view.Renderer = DC.Backbone.View.extend({
   className: 'renderer',
-  SCROLL_THROTTLE: 100,
+  SCROLL_THROTTLE: 500,
   initialize: function(options) { 
     this.createSubviews();
 
